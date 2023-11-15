5367c1ab6d515079f901280561ffefbf7a38980a
Azadeh Yousefi <azadeh.yousefim@gmail.com>
2023-11-15 23:16:20 +0330

    final attempt

---
+++ Modals/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Modals.Models
+{
+    public class Person
+    {  
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Please enter {0}")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
